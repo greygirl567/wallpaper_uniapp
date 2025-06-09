b49a5b94a66c20648723088d4ef2013007f5a6e2
greygirl567 <125628850+greygirl567@users.noreply.github.com>
2025-06-10 02:45:43 +0800

    Add files via upload

--- api/apis.js
+++ api/apis.js
@@ -1,84 +1,75 @@
-import {request} from "@/utils/request.js"
-export function apiGetBanner(){
-	// return uni.request({
-	// 	url:"https://tea.qingnian8.com/api/bizhi/homeBanner",
-	// 	header:{
-	// 		"access-key":"zqh2648"
-	// 	}
-	// })
-	
-	return request({
-		url:"/homeBanner"
-	})
+"use strict";
+const utils_request = require("../utils/request.js");
+function apiGetBanner() {
+  return utils_request.request({
+    url: "/homeBanner"
+  });
 }
-export function apiGetDayRandom(){
-	return request({url:"/randomWall"})
+function apiGetDayRandom() {
+  return utils_request.request({ url: "/randomWall" });
 }
-export function apiGetNotice(data={}){
-	return request({
-		url:"/wallNewsList",
-		data
-	})
+function apiGetNotice(data = {}) {
+  return utils_request.request({
+    url: "/wallNewsList",
+    data
+  });
 }
-export function apiGetClassify(data={}){
-	return request({
-		url:"/classify",
-		data
-	})
+function apiGetClassify(data = {}) {
+  return utils_request.request({
+    url: "/classify",
+    data
+  });
 }
-
-export function apiGetClassList(data={}){
-	return request({
-		url:"/wallList",
-		data
-	})
+function apiGetClassList(data = {}) {
+  return utils_request.request({
+    url: "/wallList",
+    data
+  });
 }
-
-export function apiGetSetupScore(data={}){
-	return request({
-		url:"/setupScore",
-		data
-	})
+function apiGetSetupScore(data = {}) {
+  return utils_request.request({
+    url: "/setupScore",
+    data
+  });
 }
-
-export function apiWriteDownload(data={}){
-	return request({
-		url:"/downloadWall",
-		data
-	})
+function apiWriteDownload(data = {}) {
+  return utils_request.request({
+    url: "/downloadWall",
+    data
+  });
 }
-
-export function apiDetailWall(data={}){
-	return request({
-		url:"/detailWall",
-		data
-	})
+function apiDetailWall(data = {}) {
+  return utils_request.request({
+    url: "/detailWall",
+    data
+  });
 }
-
-export function apiUserInfo(data={}){
-	return request({
-		url:"/userInfo",
-		data
-	})
+function apiUserInfo(data = {}) {
+  return utils_request.request({
+    url: "/userInfo",
+    data
+  });
 }
-
-export function apiGetHistoryList(data={}){
-	return request({
-		url:"/userWallList",
-		data
-	})
+function apiGetHistoryList(data = {}) {
+  return utils_request.request({
+    url: "/userWallList",
+    data
+  });
 }
-
-export function apiGetNoticeDetail(data={}){
-	return request({
-		url:"/wallNewsDetail",
-		data
-	})
+function apiSearchData(data = {}) {
+  return utils_request.request({
+    url: "/searchWall",
+    data
+  });
 }
-
-export function apiSearchData(data={}){
-	return request({
-		url:"/searchWall",
-		data
-	})
-}
+exports.apiDetailWall = apiDetailWall;
+exports.apiGetBanner = apiGetBanner;
+exports.apiGetClassList = apiGetClassList;
+exports.apiGetClassify = apiGetClassify;
+exports.apiGetDayRandom = apiGetDayRandom;
+exports.apiGetHistoryList = apiGetHistoryList;
+exports.apiGetNotice = apiGetNotice;
+exports.apiGetSetupScore = apiGetSetupScore;
+exports.apiSearchData = apiSearchData;
+exports.apiUserInfo = apiUserInfo;
+exports.apiWriteDownload = apiWriteDownload;
