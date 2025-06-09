5c4cd4eb4b3b898753e2886ffdfc6f5efd714b37
greygirl567 <125628850+greygirl567@users.noreply.github.com>
2025-06-10 02:19:57 +0800

    Add files via upload

---
+++ pages/classify/classify.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="classLayout pageBg">
+		<custom-nav-bar title="分类"></custom-nav-bar>
+		<view class="classify">
+			<theme-item v-for="item in classifyList" 
+			:key="item._id"
+			:item="item"
+			></theme-item>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import { ref } from 'vue';
+	import {onShareAppMessage,onShareTimeline} from "@dcloudio/uni-app"
+	import {apiGetClassify} from '@/api/apis.js'
+	const classifyList=ref([]);
+	
+	const getClassify = async ()=>{
+		let res = await apiGetClassify({
+			pageSize:15
+		});
+		classifyList.value = res.data
+		console.log(res);
+	}
+	getClassify();
+	
+	// 分享给好友
+	onShareAppMessage((e)=>{
+		// console.log(e);
+		return{
+			title:"壁纸 精选分类",
+			path:"/pages/classify/classify"
+		}
+	})
+	// 分享朋友圈
+	onShareTimeline(()=>{
+		return{
+			title:"壁纸 精选分类",
+			imageUrl:'/static/images/zaoqihuier.jpg'
+		}
+	})
+</script>
+
+<style lang="scss" scoped>
+.classify{
+	padding: 30rpx;
+	display: grid;
+	gap: 15rpx;
+	grid-template-columns: repeat(3,1fr);
+}
+</style>
